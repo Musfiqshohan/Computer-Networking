5c5902a38485e5e10bff30c03ed602f13eb57881
musfiqshohan <musfuq14shohan@gmail.com>
2018-04-12 13:37:12 +0600

    updated

--- Distance Vector Routing/src/Client.java
+++ Distance Vector Routing/src/Client.java
@@ -3,38 +3,58 @@ import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
-class NodeInfo{
-    String nextHop,dest;
+class NodeInfo {
+    String nextHop, dest;
     int cost;
-    NodeInfo(String dest,String nextHop, int cost)
-    {
-        this.dest=dest;
-        this.nextHop=nextHop;
-        this.cost=cost;
+
+    NodeInfo(String dest, String nextHop, int cost) {
+        this.dest = dest;
+        this.nextHop = nextHop;
+        this.cost = cost;
     }
+
+    void printNodeInfo()
+    {
+        System.out.println(dest+" "+nextHop+" "+cost);
+    }
+
 
 }
 
-class Router{
+class Router {
 
     String routername;
-    int port,cost;
-    NodeInfo routingTable[]=new NodeInfo[10];
+    int port, cost;
+    NodeInfo routingTable[] = new NodeInfo[10];
 
 
-    Router(String routername, String cost, String port)
-    {
-        this.routername=routername;
-        this.cost=Integer.valueOf(cost);
-        this.port=Integer.valueOf(port);
+    Router(int idx, String routername, String cost, String port) {
+        this.routername = routername;
+        this.cost = Integer.valueOf(cost);
+        this.port = Integer.valueOf(port);
 
-        for(int i=0;i<10;i++)
-        {
-            String dest="";
-            dest+=(char)(i+65);
-            routingTable[i]=new NodeInfo( dest , "-" , 100000);
+        for (int i = 0; i < 10; i++) {
+            String dest = "";
+            dest += (char) (i + 65);
+
+            if (i == idx)
+                routingTable[i] = new NodeInfo(dest, dest, 0);
+
+            routingTable[i] = new NodeInfo(dest, "-", 100000);
         }
     }
+
+
+
+    void printRouter()
+    {
+        System.out.println(routername+" "+port);
+        for(int i=0;i<10;i++)
+        {
+            routingTable[i].printNodeInfo();
+        }
+    }
+
 
 }
 
@@ -42,32 +62,54 @@ public class Client {
 
 
 
-
-
     public static void main(String args[]) throws IOException {
 
 
-        Router mine= new Router(args[0], "0", args[1]);
+        Scanner sc= new Scanner(System.in);
+      //  String mynode = args[0];
+        String mynode = sc.next();
+        int mynodeIdx = mynode.charAt(0) - 65;
+        System.out.println("hi");
+        //int port = Integer.valueOf(args[1]);
+        String port = (sc.next());
 
-        String filename=args[2];
+        //Router myrouter = new Router(mynodeIdx,args[0], "0", args[1]);
+        Router myrouter = new Router(mynodeIdx, mynode , "0", port);
 
-        Router neighbour[]=new Router[10];
+        //String filename = args[2];
+
+        Router neighbour[] = new Router[10];
+    //    for(int i=0;i<10;i++) neighbour[i]=new Router();
 
 
-        BufferedReader br= new BufferedReader(new FileReader("configA.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("configA.txt"));
         String line;
-        line=br.readLine();
-        int nodes= Integer.valueOf(line);
+        line = br.readLine();
+        int nodes = Integer.valueOf(line);
 
-        for(int i=0;i<nodes;i++)
-        {
-            line=br.readLine();
-            String input[]=line.split(" ");
-            neighbour[i]=new Router(input[0], input[1], input[2]);
+        for (int i = 0; i < nodes; i++) {
+            line = br.readLine();
+            System.out.println(line);
+
+            String input[] = line.split(" ");
+            int dest = input[0].charAt(0) - 65;
+            neighbour[dest] = new Router(dest,input[0], input[1], input[2]);
+            int cost = Integer.valueOf(input[1]);
+
+            myrouter.routingTable[dest] = new NodeInfo(input[0], input[0], cost);
+
+            neighbour[dest].routingTable[mynodeIdx] = new NodeInfo(mynode, mynode, cost);
+
         }
 
 
 
+//        for(int i=0;i<3;i++)
+//        {
+//            neighbour[i].printRouter();
+//            System.out.println();
+//
+//        }
 
 
 //        Socket s1 = new Socket("127.0.0.1", 1234);
